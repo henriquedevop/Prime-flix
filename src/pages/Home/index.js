9674070995330f24628e8255f3a3c5c21efe1519
henriquedevop <wymuxgh@gmail.com>
2024-11-02 17:45:11 -0300

    remove unused files, configure basic routes

---
+++ src/pages/Home/index.js
@@ -0,0 +1,9 @@
+function Home() {
+    return(
+        <div>
+            <h1>PAGINA HOME</h1>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/pages/Movie/index.js
@@ -0,0 +1,9 @@
+function Movie() {
+    return(
+        <div>
+            <h1>Seu filme é terrifier</h1>
+        </div>
+    )
+}
+
+export default Movie;
