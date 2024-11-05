a60ab44f9fdf79e974f9a54799257e48ceaf1037
henriquedevop <wymuxgh@gmail.com>
2024-11-04 21:36:38 -0300

    add favorites movies page

---
+++ src/pages/Favorites/index.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./favorites.css"
+
+function Favorites() {
+
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        
+        const userMovieList = localStorage.getItem("@henriqueflix");
+        setMovies(JSON.parse(userMovieList) || [])
+
+    },[])
+
+    function removeMovie(id) {
+        let filterMovies = movies.filter((movie) => {
+            return (movie.id != id)
+        })
+        
+        setMovies(filterMovies)
+        localStorage.setItem("@henriqueflix", JSON.stringify(filterMovies))
+    }
+
+    return(
+        <div className="userListMovies">
+            <h1>Minha lista de filmes</h1>
+
+            {movies.length === 0 && <span>Você não possui nenhum filme cadastrado :( </span>}
+
+            <ul>
+                {movies.map((movie) => {
+                    return(
+                        <li key={movie.id}>
+                            <span>{movie.title}</span>
+                            <div>
+                                <Link to={`/filme/${movie.id}`}>Ver detalhes</Link>
+                                <button onClick={() => removeMovie(movie.id)}>Excluir</button>
+                            </div>
+                        </li>
+                    )
+                })}   
+            </ul>
+        </div>
+    )
+}
+
+export default Favorites;
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Movie from "./pages/Movie";
 import Header from "./components/Header";
+import Favorites from "./pages/Favorites";
 import Error from "./pages/Error"
 
 function RoutesApp() {
@@ -12,6 +13,7 @@ function RoutesApp() {
             <Routes>
                 <Route path="/" element={ <Home/>}/>
                 <Route path="/filme/:id" element={ <Movie/>}/>
+                <Route path="/favoritos" element={ <Favorites/>}/>
 
                 <Route path="*" element={ <Error/>}/>
             </Routes>
